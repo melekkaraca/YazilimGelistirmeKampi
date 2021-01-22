e3b68c3e2dce0e3f374beb108d4c0748a97a8f65
Melek Karaca <melekkaraca@gmail.com>
2021-01-23 00:27:47 +0300

    Odev5 tamamlandı.

---
+++ 9_Odev5_Generics_Dictionary/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9_Odev5_Generics_Dictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        KeyValuePair<TKey, TValue>[] items;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<TKey, TValue>[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            if (items.Select(x=>x.Key).Contains(key) == false)
+            {
+                KeyValuePair<TKey, TValue>[] tempTuples = items;
+                items = new KeyValuePair<TKey, TValue>[items.Length + 1];
+                for (int i = 0; i < tempTuples.Length; i++)
+                {
+                    items[i] = tempTuples[i];
+                }
+                items[items.Length - 1] = KeyValuePair.Create(key, value);
+            }
+        }
+        //public int Count { get { return items.Length; } }
+    }
+}
---
+++ 9_Odev5_Generics_Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9_Odev5_Generics_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Melek");
+            myDictionary.Add(2, "Dilek");
+            myDictionary.Add(2, "abc");
+
+           
+        }
+    }
+}
